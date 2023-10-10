abe3fe75d4e6e770d150dd56d30407fe02a6c8ee
Peter Ojo <ojopeter36@gmail.com>
2023-10-10 02:18:07 +0100

    changes

--- app/main.py
+++ app/main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 from .routers import doctors, auth, patients, users, hospitals
 from .database import engine
 from . import models
-
+from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(doctors.router)
 app.include_router(users.router)
