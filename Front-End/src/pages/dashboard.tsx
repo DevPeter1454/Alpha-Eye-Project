659fde3f6b6bd7e62e328e19643530d4a8dee39a
Hadestech01 <jaolaniyi96@student.lautech.edu.ng>
2023-10-13 01:18:05 +0100

    add the add doctors page

--- Front-End/src/pages/dashboard.tsx
+++ Front-End/src/pages/dashboard.tsx
@@ -1,20 +1,35 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import DashboardLayout from "../Layouts/DashboardLayout";
 import { Tab } from "@headlessui/react";
 import Sidebar from "../components/Sidebar";
-// import AddDoctors from "../components/NewDocDefault";
+import NewDocDefault from "../components/NewDocDefault";
 import AddNewDoc from "../components/AddNewDoc";
 import ManageDoctors from "../components/ManageDoctors";
 import ViewPatients from "../components/ViewPatients";
 
-function dashboard() {
+function Dashboard() {
+  const [showAddNewDoc, setShowAddNewDoc] = useState(false);
+
+  const handleShowAddNewDocComponent = () => {
+    setShowAddNewDoc(true);
+  };
+
+  const handleCloseAddNewDocComponent = () => {
+    setShowAddNewDoc(false);
+  };
+
   return (
     <DashboardLayout>
       <Tab.Group>
         <Sidebar />
         <Tab.Panels className="w-[78%] flex justify-center">
-          {/* <AddDoctors /> */}
-          <AddNewDoc />
+          {showAddNewDoc ? (
+            <AddNewDoc handleCloseComponent={handleCloseAddNewDocComponent} />
+          ) : (
+            <NewDocDefault
+              handleShowAddNewDocComponent={handleShowAddNewDocComponent}
+            />
+          )}
           <ManageDoctors />
           <ViewPatients />
         </Tab.Panels>
@@ -23,4 +38,4 @@ function dashboard() {
   );
 }
 
-export default dashboard;
+export default Dashboard;
